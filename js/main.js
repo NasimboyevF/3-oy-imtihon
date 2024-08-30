f2aeae0da55462842b01f41c7d2bf4b44ca0d13b
NasimbayevFarhod <105714502+NasimbayevFarhod@users.noreply.github.com>
2024-08-30 14:20:28 +0500

    fl

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const Btn = document.querySelector('#nav-icon1');
+const Header = document.querySelector('.header'); 
+const body = document.querySelector('body');
+
+const mod = document.querySelector('.mod')
+
+
+  Btn.addEventListener('click', () =>{
+    body.classList.toggle('hidden')
+    Btn.classList.toggle('open');
+    Header.classList.toggle('header-burger')
+  });
+
+  mod.addEventListener('click', ()=> {
+
+    body.classList.toggle('light-mod')
+
+
+  })
+
